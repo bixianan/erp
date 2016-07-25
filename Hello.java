f584aa717ee999073b42600b56e6d8e207bb4cea
mldn <mldnqa@163.com>
2016-07-25 19:32:06 +0800

    Hello.java add two statement

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
     public static void main(String []args){
           System.out.println("Hello word!");
+          System.out.println("Œ“ «BB!");
+          System.out.println("¿≤¿≤");
     }
 }
